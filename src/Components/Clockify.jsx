395c0a500ed809bc008e9edde12aa93c0bf88b0d
Pritesh0_0 <patilpritesh347@gmail.com>
2022-09-09 12:07:45 +0530

    homepage

---
+++ src/Components/Clockify.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Homepage from "./Homepage";
+
+
+// import { NavbarF } from "../Footer/NavbarF";
+
+function Clockify() {
+  return (
+   <div style={{textAlign: "center"}}>
+   <Homepage />
+   </div>
+  );
+}
+
+export { Clockify };
