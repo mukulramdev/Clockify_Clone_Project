4922e1a92a696be42ed4ceac171222bd21ef2798
Pritesh Patil <100704776+Pritesh0-0@users.noreply.github.com>
2022-09-10 16:09:18 +0530

    Update Dashboard.jsx

--- login-signup/src/pages/Dashboard.jsx
+++ login-signup/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DashboardComponent  from '../Components/DashboardComponent'
+import DashboardComponent  from '../components/DashboardComponent'
 
 const Dashboard = () => {
     return (
